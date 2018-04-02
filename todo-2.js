0fe96171b0b6d943547a63ad03770ff746fb2c62
thomasbromehead <t_bromehead@yahoo.fr>
2018-04-02 21:23:54 +0200

    V3 build - todoList as an object

--- todo-2.js
+++ todo-2.js
@@ -1,26 +1,20 @@
-var todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-todos[0]="New Item"
-console.log(todos);
 
-function displayTodos(){
-  console.log(todos);
-}
 
-function addNewTodo(todo){
-  todos.push(todo);
-  displayTodos();
-}
-
-function changeTodo(position, newValue){
-  todos[position] = newValue;
-  displayTodos();
-}
-
-function deleteTodo(position){
-  todos.splice(position, 1);
-  displayTodos();
-}
-
-displayTodos();
-deleteTodo(0);
-changeTodo(0, "New Item 1");
+var todoList = {
+  todos : [],
+  displayTodos: function(){
+    console.log("My todos: ", this.todos);
+  },
+  addTodo: function(todo){
+    todos[position] = newValue;
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue){
+    todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
