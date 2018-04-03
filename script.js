7fc957cabe04d2be92a6c2f016af85300438fdad
thomasbromehead <t_bromehead@yahoo.fr>
2018-04-03 22:16:37 +0200

    asdakolnd

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ var view = {
   createDeleteButton : function(){
     var deleteButton = document.createElement('button');
     deleteButton.textContent = "Delete item";
-    deleteButton.style.marginLeft ="10px";
+    deleteButton.style.marginLeft ="50px";
     deleteButton.className = 'deleteButton';
     return deleteButton;
   },
